b0bdb00065763a98112e4d2575b25c52fc29af3e
Andres Alvarez <andres@endor>
2015-09-12 06:33:00 -0430

    Updated module

--- tasks/depsconcat.js
+++ tasks/depsconcat.js
@@ -1,69 +1,73 @@
-var path, util, deptree;
-
-path = require('path');
-util = require('util');
-deptree = require('serialize-deptree');
-
 module.exports = function(grunt) {
-  grunt.registerMultiTask('depsconcat', 'Concatenate files, ordered by dependencies', function() {
+  var path, util, deptree;
+
+  path = require('path');
+  util = require('util');
+  deptree = require('serialize-deptree');
+
+  grunt.registerMultiTask('depsconcat', 'Concatenate files, ordered by dependencies.', function() {
     var options;
 
     options = this.options({
       separator: grunt.util.linefeed,
       requireTemplate: null,
-      ext: null,
-      except: [],
+      ext: null
     });
 
-    this.files.forEach(function(file) {
-      var regex, ext, files, tree;
+    this.files.forEach(function(f) {
+      var ext, files, tree, regex, serialized;
+      var filePath, i;
 
-      ext = options.ext || file.dest.match(/\.[a-z]+$/i)[0];
+      ext = options.ext || f.dest.match(/\.[a-z]+$/i)[0];
       files = {};
       tree = {};
 
       if (!options.requireTemplate) {
-        if (ext === '.css') {
+        if (ext === '.css')
           regex = '@import\\s+url\\(["\']?([^"\'()]+)["\']?\\);?[\\n\\r]*';
-        } else {
+        else {
           ext = '.js';
           regex = '\\/\\/@require\\s+([^\\n\\r]+)[\\n\\r]*';
         }
-      } else {
+      } else
         regex = options.requireTemplate;
-      }
 
       regex = new RegExp(regex, 'gi');
 
-      file.src.filter(function(filePath) {
-        filePath = path.normalize(filePath);
+      f.src.filter(function(filePath) {
+        if (!grunt.file.exists(filePath)) {
+          grunt.log.warn('Source file ' + filepath + ' not found.');
+          return false;
+        }
 
+        return !grunt.file.isDir(filePath);
+      }).map(function(filePath) {
         files[path.basename(filePath, ext)] = filePath;
         tree[filePath] = [];
       });
 
-      file.src.filter(function(filePath) {
-        var matches, depfilePath;
+      for (filePath in tree) {
+        var matches, name;
 
         matches = grunt.file.read(filePath).match(regex) || [];
 
-        for (var i = 0; i < matches.length; i++) {
+        for (i = 0; i < matches.length; i++) {
           regex.lastIndex = 0;
-          depfilePath = regex.exec(matches[i])[1];
+          name = regex.exec(matches[i])[1];
 
-          if (!(/\.\w+$/).test(depfilePath)) {
-            tree[path.normalize(filePath)].push(files[depfilePath]);
-          }
+          if (files[name])
+            tree[filePath].push(files[name]);
+          else
+            grunt.log.warn('Dependency ' + name + ' of ' + filePath + ' not found.');
         }
-      });
+      }
 
-      var serialized = deptree.serialize(tree).map(function(filePath) {
+      serialized = deptree.serialize(tree).map(function(filePath) {
         return grunt.file.read(filePath);
       });
 
-      grunt.file.write(file.dest, serialized.join(grunt.util.linefeed));
-      grunt.log.writeln('Dest File "' + file.dest + '" created.');
+      grunt.file.write(f.dest, serialized.join(grunt.util.linefeed));
+      grunt.log.writeln('File ' + f.dest + ' created.');
     });
   });
-
 };
