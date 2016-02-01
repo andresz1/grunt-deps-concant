f6856ea3d6646aa1c1284b101fc2fc2c9d7a351e
Andres Alvarez <andres@endor>
2016-02-01 09:42:17 -0430

    Added nameTemplate

--- tasks/depsconcat.js
+++ tasks/depsconcat.js
@@ -11,49 +11,64 @@ module.exports = function(grunt) {
     options = this.options({
       separator: grunt.util.linefeed,
       requireTemplate: null,
+      nameTemplate: null,
       ext: null
     });
 
     this.files.forEach(function(f) {
-      var ext, files, tree, regex, serialized;
+      var ext, files, tree, requireRegex, nameRegex, serialized;
       var filePath, i;
 
       ext = options.ext || f.dest.match(/\.[a-z]+$/i)[0];
       files = {};
       tree = {};
+      nameRegex = null;
 
       if (!options.requireTemplate) {
         if (ext === '.css')
-          regex = '@import\\s+url\\(["\']?([^"\'()]+)["\']?\\);?[\\n\\r]*';
+          requireRegex = '@import\\s+url\\(["\']?([^"\'()]+)["\']?\\);?[\\n\\r]*';
         else {
           ext = '.js';
-          regex = '\\/\\/@require\\s+([^\\n\\r]+)[\\n\\r]*';
+          requireRegex = '\\/\\/@require\\s+([^\\n\\r]+)[\\n\\r]*';
         }
       } else
-        regex = options.requireTemplate;
+        requireRegex = options.requireTemplate;
 
-      regex = new RegExp(regex, 'gi');
+      requireRegex = new RegExp(requireRegex, 'gi');
+
+      if (options.nameTemplate)
+        nameRegex = new RegExp(options.nameTemplate, 'gi');
 
       f.src.filter(function(filePath) {
         if (!grunt.file.exists(filePath)) {
-          grunt.log.warn('Source file ' + filepath + ' not found.');
+          grunt.log.warn('Source file ' + filePath + ' not found.');
           return false;
         }
 
         return !grunt.file.isDir(filePath);
       }).map(function(filePath) {
-        files[path.basename(filePath, ext)] = filePath;
+        var matches;
+
+        if (nameRegex) {
+          nameRegex.lastIndex = 0;
+          matches = grunt.file.read(filePath).match(nameRegex) || [];
+
+          if (matches.length)
+            files[nameRegex.exec(matches[0])[1]] = filePath;
+        } else
+          files[path.basename(filePath, ext)] = filePath;
+
         tree[filePath] = [];
       });
 
       for (filePath in tree) {
         var matches, name;
 
-        matches = grunt.file.read(filePath).match(regex) || [];
+        matches = grunt.file.read(filePath).match(requireRegex) || [];
 
         for (i = 0; i < matches.length; i++) {
-          regex.lastIndex = 0;
-          name = regex.exec(matches[i])[1];
+          requireRegex.lastIndex = 0;
+          name = requireRegex.exec(matches[i])[1];
 
           if (files[name])
             tree[filePath].push(files[name]);
